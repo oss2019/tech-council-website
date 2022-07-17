0e4f0829a6a68d068b3065c57549f01fd68da530
Shriagrawal <99610153+Shriagrawal@users.noreply.github.com>
2022-07-17 14:27:33 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "./component/Navbar";
+import Jumbotron from "./component/Jumbotron";
+
+
+import './App.css';
+function App() {
+  return (
+    <>
+  <Jumbotron />
+  <Navbar />
+    </>
+  );
+}
+export default App;
+
+
